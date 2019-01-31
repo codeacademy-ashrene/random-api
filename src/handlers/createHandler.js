c04c19c42c4e4a16893922a780cbedc1e2b8108c
Ashrene Roy <ashrene_roy@mckinsey.com>
2019-01-31 21:30:57 +0530

    Handler: Modified create handler to get data from api, and request payload into an object

--- src/handlers/createHandler.js
+++ src/handlers/createHandler.js
@@ -1,3 +1,23 @@
-const createHandler = (request, h) => h.response(request.payload);
+const axios = require('axios');
+
+const getDatafromRandomApi = () => {
+  try {
+    return axios.get('http://gturnquist-quoters.cfapps.io/api/random').then(response => (response.data));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const createHandler = async (request, h) => {
+  const randomApiData = await getDatafromRandomApi();
+  const payloadData = {
+    name: request.payload.name,
+    email: request.payload.email,
+    noOfQuery: request.payload.noOfQuery,
+    randomQuery: randomApiData.value.quote,
+  };
+  return h.response(payloadData);
+};
+
 
 module.exports = { createHandler };
